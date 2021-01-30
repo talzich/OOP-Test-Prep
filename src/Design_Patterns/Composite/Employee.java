5fa3615d1405a15df149801c2bb569c92e813206
Tal <talzhtc@gmail.com>
2021-01-31 00:24:10 +0200

    Added documantation

--- src/Design_Patterns/Composite/Employee.java
+++ src/Design_Patterns/Composite/Employee.java
@@ -1,15 +1,24 @@
 package Design_Patterns.Composite;
 
+/**
+ * This class represents the leaf part of the composite design pattern
+ * It stores the data of a single employee and implements displayEmployee()
+ * from the EmployeeComponent abstract class.
+ */
 public class Employee extends EmployeeComponent{
 
     private String name, title, department;
 
+    // A simple constructor
     public Employee(String name, String department, String title){
         this.name = name;
         this.department = department;
         this.title = title;
     }
 
+    /**
+     * From the EmployeeComponent interface
+     */
     @Override
     public void displayEmployeeData() {
         System.out.println("Title: " +this.title +"\n\t Department: " +this.department +"\n\t Name: " + this.name);
