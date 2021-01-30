d5fb3698a3574c9072f6bdd8875e7ed57c036457
Tal <talzhtc@gmail.com>
2021-01-31 00:33:52 +0200

    Added documentation

--- src/Design_Patterns/Composite/Firm.java
+++ src/Design_Patterns/Composite/Firm.java
@@ -2,19 +2,24 @@ package Design_Patterns.Composite;
 
 import java.util.ArrayList;
 
+/**
+ * This class facilitates the composite design pattern.
+ * It stores all of its composites and leaves.
+ */
 public class Firm {
 
     private String name, city;
     private ArrayList<String> departments;
     private ArrayList<EmployeeComponent> employees;
 
+    // Simple constructor
     public Firm(String name, String city){
         this.name = name;
         this.city = city;
         this.departments = new ArrayList<>();
         this.employees = new ArrayList<>();
     }
-
+    
     public void addDepartment(String department){
         this.departments.add(department);
     }
